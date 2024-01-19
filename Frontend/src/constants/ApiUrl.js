b34b960d0609cdcba36a6e75ac9f1689679f0e05
shubham <shubham.deshpande@vernost.in>
2024-01-19 22:28:48 +0530

    path change

--- Frontend/src/constants/ApiUrl.js
+++ Frontend/src/constants/ApiUrl.js
@@ -1,9 +1,9 @@
-export let BASE_URL = "http://34.230.50.80";
+export let BASE_URL = "http://34.230.50.80/api/";
 // export let BASE_URL = "";
 // export const GET_MOVIE_LIST_API_URL = `${BASE_URL}/Auth/get_banners`;
-export const LOGIN_API_URL = `${BASE_URL}api/user/user-signin`;
-export const ADD_MOVIE_API_URL = `${BASE_URL}api/movie/create-movie`;
-export const UPDATE_MOVIE_API_URL = `${BASE_URL}api/movie/update-movie/`;
+export const LOGIN_API_URL = `${BASE_URL}user/user-signin`;
+export const ADD_MOVIE_API_URL = `${BASE_URL}movie/create-movie`;
+export const UPDATE_MOVIE_API_URL = `${BASE_URL}movie/update-movie/`;
 
-export const GET_MOVIE_LIST_API_URL = `${BASE_URL}api/movie/movie-list`;
-export const GET_SINGLE_MOVIE_API_URL = `${BASE_URL}api/movie/movie-by-id/`;
+export const GET_MOVIE_LIST_API_URL = `${BASE_URL}movie/movie-list`;
+export const GET_SINGLE_MOVIE_API_URL = `${BASE_URL}movie/movie-by-id/`;
